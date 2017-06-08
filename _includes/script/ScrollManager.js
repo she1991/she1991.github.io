05f6da70aea031cfaf0af0f6a6276a96f06a716c
Akash <akash@Akashs-MacBook-Air.local>
2017-06-07 20:50:16 -0500

    Added full page scrolling

--- _includes/script/ScrollManager.js
+++ _includes/script/ScrollManager.js
@@ -1,14 +1,33 @@
 var currentPage;
 var backPage;
+var lastScrollTop = 0;
 
 function bodyLoaded(){
+	$(document).ready(function() {
+		$('#fullpage').fullpage();
+	});
     //Get all post divs
     var postDivs = d3.selectAll(".post");
     //Display the first post div
     postDivs.style("visibility",
-        function(d, i){if(i == 0){return "visible";}else{return "hidden"}}
+        function(d, i){if(i == 0){return "visible";}else{return "visible"}}
     );
     //Make 'posts' div visible
     var posts = d3.selectAll(".posts")
         .style("visibility", "visible");
+    lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    window.addEventListener('scroll', scrollHandler, true)
+}
+
+function scrollHandler(e) {
+	console.log("scroll", e);
+	var st = window.pageYOffset || document.documentElement.scrollTop;
+   	if (st > lastScrollTop){
+    	// downscroll code
+    	console.log("down");
+	} else {
+		// upscroll code
+		console.log("up");
+	}
+	lastScrollTop = st;
 }
