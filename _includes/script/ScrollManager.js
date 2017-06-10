3554f4379c42a11c4c1a261028916488be0883b4
Akash <akash@Akashs-MacBook-Air.local>
2017-06-09 23:42:08 -0500

    Changed scroll trigger colors added scroll beads

--- _includes/script/ScrollManager.js
+++ _includes/script/ScrollManager.js
@@ -1,7 +1,3 @@
-var currentPage;
-var backPage;
-var lastScrollTop = 0;
-
 function bodyLoaded(){
 	$(document).ready(function() {
 		$('#fullpage').fullpage();
@@ -21,11 +17,24 @@ function updateFixedDivs() {
     if(!nextPost.empty()){
         nextPostTitle = String(nextPost.select(".post-title").text()).trim();
     }
-    console.log(activePostTitle, nextPostTitle);
     //Set activePostTitle to scroll-up-trigger
     d3.select(".scroll-up-trigger h4")
         .text(activePostTitle);
     //Set nextPostTitle to scroll-down-trigger
-    d3.select(".scroll-down-trigger h4")
-        .text(nextPostTitle+".");
+    if(nextPostTitle.length == 0){
+        //Hide scroll arrow
+        d3.select(".scroll-down-trigger .scroll-arrow")
+            .attr("class", "hidden-scroll-arrow");
+        d3.select(".scroll-down-trigger h4")
+            .attr("class", "the-end-trigger")
+            .text("The End.");
+    }
+    else {
+        d3.select(".scroll-down-trigger .hidden-scroll-arrow")
+            .attr("class", "scroll-arrow");
+        d3.select(".the-end-trigger")
+            .attr("class","");
+        d3.select(".scroll-down-trigger h4")
+            .text(nextPostTitle+".");
+    }
 }
